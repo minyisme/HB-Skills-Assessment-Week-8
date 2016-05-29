e56fb215bea319a8c19ccbd3b54e13aabd31d3df
Min Yang <maggieyang@Mins-MacBook-Pro.local>
2016-05-29 12:13:38 -0700

    finished hw

---
+++ discussion_questions.py
@@ -0,0 +1,22 @@
+#Discussion Questions:
+
+#Runtime
+#1. O(n)/linear. 
+#2. O(1), O(log n), O(n log n), O(n), O(n**2), O(2**n)
+
+#Stacks and Queues
+#1.1 Stack
+#1.2 Queue
+#1.3 Neither or both...depending on how much the stack/queue knows about order of operations?
+#2. customer support tickets, voter registration processing
+#3. getting in and out of a minivan, loading/unloading passengers on plane
+
+#Linked Lists
+#1. Nodes: boxes containing "Apple", "Berry", "Cherry" and the nexts; Data for each node: "Apple", "Berry", "Cherry"; Head: node containing data "Apple" and next pointer; Tail: node containing data "Cherry" and next pointer; 
+#2. Singly linked list only points one direction e.g. from head to tail. A doubly linked list points in both directions from head to tail and from tail to head.
+#3. Becasue we can just point the next pointer on tail to new node and call that the tail. Otherwise, we'll have to trace all the way from the head through the next pointers to the last one and then add to it.
+
+#Trees
+#1. food, Italian, Indian, Mexican, lasagna, pizza, tikka masala, saag, burrito
+#2. food, Italian, lasagna, pizza, thin crust, Chicago-style
+#3. Binary search tree only has at most two pointers coming from each node.
